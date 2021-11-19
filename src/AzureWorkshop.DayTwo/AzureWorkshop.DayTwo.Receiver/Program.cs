33d9f118a3fd19865dc04fb4c89709cb6afe53f0
Van Eynde Stijn - Xerius <stijn.vaneynde@xerius.be>
2021-11-19 09:39:27 +0100

    Setup

---
+++ src/AzureWorkshop.DayTwo/AzureWorkshop.DayTwo.Receiver/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+
+namespace AzureWorkshop.DayTwo.Receiver
+{
+    class Program
+    {
+        private static readonly string _connectionString = "";
+        private static readonly string _topicName = "some-topic-name";
+        private static readonly string _subscriptionName = Environment.MachineName;
+
+        static async Task Main(string[] args)
+        {
+            await EnsureTopicAsync();
+            await EnsureSubscriptionAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task EnsureTopicAsync()
+        {
+        }
+
+        private static async Task EnsureSubscriptionAsync()
+        {
+        }
+    }
+}
---
+++ src/AzureWorkshop.DayTwo/AzureWorkshop.DayTwo.Sender/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AzureWorkshop.DayTwo.Sender
+{
+    class Program
+    {
+        private static readonly string _connectionString = "";
+        private static readonly string _topicName = "some-topic-name";
+
+        static async Task Main(string[] args)
+        {
+            await EnsureTopicAsync();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task EnsureTopicAsync()
+        {
+        }
+    }
+}
